a5c166f2e731b7aa2635e8137ac8527a9f766e00
AnatolySolovyev12 <167902367+AnatolySolovyev12@users.noreply.github.com>
2025-12-17 21:52:23 +0300

    add minimal func

--- MigrationApp/main.cpp
+++ MigrationApp/main.cpp
@@ -1,16 +1,24 @@
 #include <QtCore/QCoreApplication>
 #include <QSqlDatabase>
 #include <QSqlError>
-
+#include <qsqlquery>
 
 void connectDataBase();
+void getTablesArray();
+QString getDataBaseName(QString paramString);
 
+
+
+QList<QString>stringTablesArray;
+QSqlDatabase mainDb;
 
 int main(int argc, char* argv[])
 {
 	QCoreApplication app(argc, argv);
 
 	connectDataBase();
+	
+
 
 	return app.exec();
 }
@@ -18,14 +26,25 @@ int main(int argc, char* argv[])
 
 void connectDataBase()
 {
-	QSqlDatabase mainDb = QSqlDatabase::addDatabase("QPSQL"); // Работаем используя библиотеки PostgreSQL
+	/*
+	mainDb = QSqlDatabase::addDatabase("QPSQL"); // Работаем используя библиотеки (PostgreSQL) и отдельные методы для подключения. Вкладываем их потом в папку с программой.
 	mainDb.setHostName("192.168.56.101");
 	mainDb.setDatabaseName("testdb");
 	mainDb.setUserName("solovev");
-	mainDb.setPassword("art5401121");
+	mainDb.setPassword("art54011212");
 	mainDb.setPort(5432);  // По умолчанию 5432
+	*/
 
-	//QSqlDatabase mainDb = QSqlDatabase::addDatabase("QODBC"); //  Работаем через ODBC
+	QSqlDatabase mainDb = QSqlDatabase::addDatabase("QODBC"); // Работаем используя QODBC (SQL Server) и отдельную строку. Методы не сработают. 
+	mainDb.setDatabaseName(
+		"DRIVER={SQL Server};"
+		"Server=127.0.0.1,1433;"
+		"Database=testdb;"
+		"Uid=solovev;"
+		"Pwd=art54011212;"
+	); //"DataBase is CONNECT to DRIVER={SQL Server};Server=127.0.0.1,1433;Database=testdb;Uid=solovev;Pwd=art54011212;"
+
+	//QSqlDatabase mainDb = QSqlDatabase::addDatabase("QODBC"); //  Работаем через QODBC в таком варианте для подключения напрямую к PostgreSQL (работает без отдельных библиотек)
 	//mainDb.setDatabaseName("DRIVER={PostgreSQL ANSI};SERVER=192.168.56.101;PORT=5432;DATABASE=testdb;UID=solovev;PWD=art54011212");
 
 	if (!mainDb.open())
@@ -41,6 +60,41 @@ void connectDataBase()
 	}
 	else
 	{
-		qDebug() << "DataBase is CONNECT.";
+		qDebug() << "DataBase is CONNECT to " + getDataBaseName(mainDb.databaseName());
 	}
 }
+
+void getTablesArray()
+{
+	QSqlQuery tablesQuery;
+
+	QString dbNameFromConnect = mainDb.databaseName();
+
+	tablesQuery.prepare(R"(
+SELECT TABLE_CATALOG
+FROM :dbNameFromConnect.INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'dbo' AND TABLE_TYPE = 'BASE TABLE'
+        )");
+
+	tablesQuery.bindValue(":dbNameFromConnect", dbNameFromConnect);
+
+	if (!tablesQuery.exec() || !tablesQuery.next())
+	{
+		tablesQuery.lastError().text();
+	}
+	else
+	{
+		do {
+			stringTablesArray.push_back(tablesQuery.value(0).toString());
+		} while (tablesQuery.next());
+	}
+}
+
+QString getDataBaseName(QString paramString)
+{
+	QString temp = paramString.sliced(paramString.indexOf("Database=")).sliced(9);
+    temp = temp.left(temp.indexOf(';'));
+	
+	return temp;
+}
+
